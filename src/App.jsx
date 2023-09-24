7bfd6a99963e2dc6b6fb01cdb33de2751ee94592
nuggettempura <adammustaqeemhalid@gmail.com>
2023-09-24 13:34:12 +0800

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,39 @@
+import { useState } from 'react';
+import Button from "react-bootstrap/Button";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function TodoList({todos}) {
+  console.log(todos)
+  return (
+    <ul>
+      {todos.map((todo, index) => {
+        return <li key={index}>{todo}</li>
+      })}
+    </ul>
+  )
+}
+
+function App() {
+  const [todos, setTodos] = useState([]);
+  const [newTodo, setNewTodo] = useState("");
+  function addTodo() {
+    setTodos([...todos, newTodo]);
+    setNewTodo("");
+  }
+
+  return (
+      <div className="m-3">
+        <input 
+          type="text" 
+          value={newTodo}
+          onChange={(event) => setNewTodo(event.target.value)}
+        />
+        <Button variant="primary" className="ms-3" onClick={addTodo}>
+          Add
+        </Button>
+        <TodoList todos={todos} />
+      </div>
+  );
+}
+
+export default App
